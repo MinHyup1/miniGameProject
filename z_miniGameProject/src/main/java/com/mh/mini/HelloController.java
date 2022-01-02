909e8f49b129fb0693482393a3a07822e146cb40
MinHyup1 <kkmh7511@naver.com>
2022-01-02 21:40:04 +0900

    프로젝트 시작

---
+++ z_miniGameProject/src/main/java/com/mh/mini/HelloController.java
@@ -0,0 +1,14 @@
+package com.mh.mini;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+
+	@GetMapping("/")
+	public String index() {
+		return "Greet ings from Strping Boot!! 미니게임 프로젝트!";
+	}
+}
---
+++ z_miniGameProject/src/main/java/com/mh/mini/ZMiniGameProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mh.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZMiniGameProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZMiniGameProjectApplication.class, args);
+	}
+
+}
