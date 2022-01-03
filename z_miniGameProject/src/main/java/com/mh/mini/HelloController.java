5be4b4db6701054f663222bf8aa7eda35e55a998
MinHyup1 <kkmh7511@naver.com>
2022-01-03 16:52:24 +0900

    오라클 db 셋팅

--- z_miniGameProject/src/main/java/com/mh/mini/HelloController.java
+++ z_miniGameProject/src/main/java/com/mh/mini/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-
 	@GetMapping("/")
 	public String index() {
 		return "Greet ings from Strping Boot!! 미니게임 프로젝트!";
--- z_miniGameProject/src/main/java/com/mh/mini/ZMiniGameProjectApplication.java
+++ z_miniGameProject/src/main/java/com/mh/mini/ZMiniGameProjectApplication.java
@@ -1,5 +1,6 @@
 package com.mh.mini;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
